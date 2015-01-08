b5238664fdc00c608dee73b90f8c95683d0deba5
JesonRondo <pancnlz@hotmail.com>
2015-01-08 14:18:18 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,15 @@
 #! /bin/sh
+
+# gcc
+yum -y install gcc gcc-c++ openssl-devel
+y
+
+# node
+mkdir -p /tmp/pkg && cd /tmp/pkg
+wget http://nodejs.org/dist/v0.10.24/node-v0.10.24.tar.gz
+tar -xzvf node-v0.10.24.tar.gz
+cd node-v0.10.24
+./configure --prefix=/usr/local/node
+make && make install
+
+npm install -g n
