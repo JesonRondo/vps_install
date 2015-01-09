7564bc3e0df53be0766d718e07e2da9b7696149c
JesonRondo <pancnlz@hotmail.com>
2015-01-09 10:18:20 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,9 +6,9 @@ y
 
 # node
 mkdir -p /tmp/pkg && cd /tmp/pkg
-wget http://nodejs.org/dist/v0.10.35/node-v0.10.35-linux-x64.tar.gz
-tar -xzvf node-v0.10.35-linux-x64.tar.gz
-cd node-v0.10.35-linux-x64
+wget http://nodejs.org/dist/v0.10.35/node-v0.10.35.tar.gz
+tar -xzvf node-v0.10.35.tar.gz
+cd node-v0.10.35
 ./configure
 make && make install
 
